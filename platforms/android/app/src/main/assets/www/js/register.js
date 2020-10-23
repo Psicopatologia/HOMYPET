c08305f3d3e83483e793a4b22b0119fbb6b9e453
Sebastián Orozco <44331089+Psicopatologia@users.noreply.github.com>
2020-10-23 00:55:20 -0500

    Se añadió la plataforma navegador

---
+++ platforms/android/app/src/main/assets/www/js/register.js
@@ -0,0 +1,12 @@
+// Name and Password from the register-form
+var email = document.getElementById('email');
+var confEmail = document.getElementById('confEmail');
+var pass = document.getElementById('pass');
+var terms = document.getElementById('terms');
+
+// storing input from register-form
+function store() {
+    alert("oh no")
+    // localStorage.setItem('name', name.value);
+    // localStorage.setItem('pw', pw.value);
+}
